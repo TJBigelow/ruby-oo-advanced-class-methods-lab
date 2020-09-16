9e705e516117a773ca29e7023ff38623c329d52a
tjbigelow <tjbigelow@att.net>
2020-09-15 20:32:26 -0500

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class Song
   attr_accessor :name, :artist_name
   @@all = []
@@ -10,4 +11,55 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    @song = self.new
+    self.all.push(@song)
+    @song
+  end
+
+  def self.new_by_name(song_name)
+    @song = self.new
+    @song.name = song_name
+    @song
+  end
+
+  def self.create_by_name(song_name)
+    @song = self.create
+    @song.name = song_name
+    @song
+  end
+
+  def self.find_by_name(song_name)
+    self.all.find{|song| song.name == song_name}
+  end
+
+  def self.find_or_create_by_name(song_name)
+    self.find_by_name(song_name) || self.create_by_name(song_name)
+  end
+
+  def self.alphabetical
+    self.all.sort_by{|song| song.name}
+  end
+
+  def self.new_from_filename(filename)
+    @song = self.new
+    @array = filename.split(' - ')
+    @array.each {|value| value.delete_suffix!('.mp3')}
+    @song.artist_name = @array[0]
+    @song.name = @array[1]
+    @song
+  end
+
+  def self.create_from_filename(filename)
+    @song = self.create
+    @array = filename.split(' - ')
+    @array.each {|value| value.delete_suffix!('.mp3')}
+    @song.artist_name = @array[0]
+    @song.name = @array[1]
+    @song
+  end
+
+  def self.destroy_all
+    self.all.clear
+  end
 end
